9a0832407c1cee6b5ad163434e3958535d4edf84
Lukman <manglayangselalu@gmail.com>
2026-06-03 14:45:10 +0700

    update func init

--- story_content/user.js
+++ story_content/user.js
@@ -2,6 +2,29 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "6Wpbto5IkV9":
+        Script1();
+        break;
+      case "6ee27ccBR5R":
+        Script2();
+        break;
   }
+}
+
+function Script1()
+{
+  if(!window.bgMusic).{
+window.bgMusic = new Audio("story_content/bgmusic.mp3");
+window.bgMusic.loop =false;
+window.bgMusic.volume = 0.3;
+window.bgMusic.play();
+}
+}
+
+function Script2()
+{
+  if (window.bgMusic) {
+window.bgMusic.muted = !window.bgMusic.muted;
+}
 }
 
